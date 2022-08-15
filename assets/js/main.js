903497f5a4836c62440c6e642b6b4b0167b8a346
MD <dhmedy@googlemail.com>
2022-08-15 13:48:10 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+/*
+Array names mit den Werten: "Eric", "Steffen", "Finn", "Simon", "Franzi", "Kim", "Sergio".
+*/
+
+let arrayNames = ["Eric", "Steffen", "Finn", "Simon", "Franzi", "Kim", "Sergio"];
+
+for (let i = 0; i < arrayNames.length; i++) {
+    console.log('index : ', i, 'Name : ', arrayNames[i]);
+}
